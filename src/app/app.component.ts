aaf1532edf85d23d040270ac90bf530960be4700
Alex Tinoco <tinocoalex003@gmail.com>
2024-11-12 09:06:07 +0100

    salut la team

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Medecin } from './types/medecin';
 import { DoctorsService } from './services/doctors.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { DoctorComponent } from './doctor/doctor.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DoctorComponent],
+  imports: [RouterOutlet, DoctorComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,12 +17,27 @@ export class AppComponent implements OnInit {
   private readonly doctorsService = inject(DoctorsService);
   private readonly destroyRef = inject(DestroyRef);
   medecins!: Medecin[];
+  filteredMedecins!: Medecin[];
+
+  searchDoctorControl: FormControl<string | null> = new FormControl('');
 
   ngOnInit(): void {
     this.doctorsService.getDoctors()
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((medecins) => {
       this.medecins = medecins;
+      this.filteredMedecins = medecins;
     });
   }
+
+  searchDoctor(){
+    const name = this.searchDoctorControl.value;
+
+    if (!name){
+      this.filteredMedecins = this.medecins;
+      return;
+    }
+
+  }
 }
+
